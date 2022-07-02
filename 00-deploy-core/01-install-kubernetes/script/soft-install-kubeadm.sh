e1fe376d885e8ee78a50499c58edb15ba3bce9b7
OpenEdgn <ExplodingFKL@gmail.com>
2022-07-02 15:33:04 +0800

    doc: 更新文档

--- 00-deploy-core/01-install-kubernetes/script/soft-install-kubeadm.sh
+++ 00-deploy-core/01-install-kubernetes/script/soft-install-kubeadm.sh
@@ -3,7 +3,7 @@ apt-get install -y apt-transport-https ca-certificates curl
 # 导入公钥对
 curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 # 配置 APT
-echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | tee /etc/apt/sources.list.d/kubernetes.list
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://mirrors.ustc.edu.cn/kubernetes/apt/ kubernetes-xenial main" | tee /etc/apt/sources.list.d/kubernetes.list
 apt-get update
 # 安装 kubeadm
 apt-get install -y kubelet kubeadm kubectl cri-tools
