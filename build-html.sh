84f7d5dc7285ea02585d482a3306ace5990934c7
OpenEdgn <ExplodingFKL@gmail.com>
2022-07-15 00:44:35 +0800

    feat: MariaDB 部署完成。

--- build-html.sh
+++ build-html.sh
@@ -20,10 +20,17 @@ for data in **/*.adoc; do
     SRC_FILE_NAME="$(basename "${SRC_PATH}")"
     DIST_FILE_NAME=${SRC_FILE_NAME//.adoc/.html}
     mkdir -p "$SRC_DIRECTORY" || : 2>/dev/null
-#    echo $SRC_DIRECTORY
+    #    echo $SRC_DIRECTORY
     asciidoctor --safe-mode unsafe -r asciidoctor-kroki --out-file "$SRC_DIRECTORY/$DIST_FILE_NAME" "$SRC_DIRECTORY/$SRC_FILE_NAME"
     sed -i 's/.adoc">/.html">/g' "$SRC_DIRECTORY/$DIST_FILE_NAME"
 done
+/bin/rm -f "$OLD_BUILD_DIR"/zz-MENU.html
+asciidoctor --safe-mode unsafe -r asciidoctor-kroki --no-header-footer \
+    --out-file "$OLD_BUILD_DIR"/zz-MENU.html "$OLD_BUILD_DIR"/zz-MENU.adoc
+sed -i 's/.adoc">/.html">/g' "$OLD_BUILD_DIR"/zz-MENU.html
+sed -i 's/<a href="/<a target="dist" href="/g' "$OLD_BUILD_DIR"/zz-MENU.html
+
+
 mv "$OLD_BUILD_DIR"/* "$BUILD_DIR"/
-cp "$BUILD_DIR"/README.html "$BUILD_DIR"/index.html
+#cp "$BUILD_DIR"/README.html "$BUILD_DIR"/index.html
 echo "build finish ！ dist dir : $BUILD_DIR"/ .
