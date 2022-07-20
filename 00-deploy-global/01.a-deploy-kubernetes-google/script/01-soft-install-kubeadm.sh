a29afd2415c5e7ee0281020cfdd8f36435f7b054
ExplodingDragon <ExplodingDragon@users.noreply.github.com>
2022-07-20 07:55:53 +0000

    deploy: 74e2ac6b4a8beb066c952de6ce4a7710dd2a4aca

--- 00-deploy-global/01.a-deploy-kubernetes-google/script/01-soft-install-kubeadm.sh
+++ 00-deploy-global/01.a-deploy-kubernetes-google/script/01-soft-install-kubeadm.sh
@@ -11,6 +11,8 @@ KUBE_RELEASE_VERSION="v0.14.0"
 #下载目录
 DOWNLOAD_PATH=/tmp/kubernetes
 mkdir -p $DOWNLOAD_PATH
+IMAGE_PATH=$DOWNLOAD_PATH/images
+mkdir -p $IMAGE_PATH
 
 # 准备环境
 wget $KUBERNETES_BIN_URL/$KUBEADM_RELEASE/bin/linux/$ARCH/kubeadm -c -O $DOWNLOAD_PATH/kubeadm
@@ -21,13 +23,22 @@ wget "https://raw.githubusercontent.com/kubernetes/release/$KUBE_RELEASE_VERSION
 wget "https://raw.githubusercontent.com/kubernetes/release/$KUBE_RELEASE_VERSION/cmd/kubepkg/templates/latest/deb/kubeadm/10-kubeadm.conf" -c -O $DOWNLOAD_PATH/10-kubeadm.conf
 chmod +x $DOWNLOAD_PATH/kubeadm
 # 下载镜像
+save_image() {
+    docker pull "$1"
+    # shellcheck disable=SC2001
+    docker save -o $IMAGE_PATH/"$(echo "$1" | sed s@/@-@g)" "$1"
+}
+# 导入默认镜像
 IFS=" " read -r -a LIST <<<"$(HTTP_PROXY=no HTTPS_PROXY=no $DOWNLOAD_PATH/kubeadm config images list 2>/dev/null | xargs echo)"
 # shellcheck disable=SC2128
 for IMAGE in ${LIST[*]}; do
-    docker pull "$IMAGE"
-    # shellcheck disable=SC2001
-    docker save -o $DOWNLOAD_PATH/"$(echo "$IMAGE" | sed s@/@-@g)" "$IMAGE"
+    save_image "$IMAGE"
 done
+
+# 导入自定义镜像
+save_image "k8s.gcr.io/pause:3.6"
+save_image "rancher/mirrored-flannelcni-flannel-cni-plugin:v1.1.0"
+save_image "rancher/mirrored-flannelcni-flannel:v0.18.1"
 
 # 安装
 tar -C $DOWNLOAD_PATH -zxf $DOWNLOAD_PATH/cri-tools.tgz
@@ -43,8 +54,22 @@ install -m 644 $DOWNLOAD_PATH/10-kubeadm.conf /etc/systemd/system/kubelet.servic
 systemctl daemon-reload
 systemctl enable --now kubelet
 
+# 配置 cri
+cat <<EOF | tee /etc/crictl.yaml
+runtime-endpoint: unix:///var/run/containerd/containerd.sock
+image-endpoint: unix:///var/run/containerd/containerd.sock
+EOF
+
 # 导入镜像
-IFS=" " read -r -a LIST <<<"$(HTTP_PROXY=no HTTPS_PROXY=no /usr/bin/kubeadm config images list 2>/dev/null | xargs echo)"
+# shellcheck disable=SC2178
+LIST=$(
+    cd $IMAGE_PATH || exit
+    # shellcheck disable=SC2035
+    ls *
+)
 for IMAGE in ${LIST[*]}; do
-    ctr image import $DOWNLOAD_PATH/"$(echo "$IMAGE" | sed s@/@-@g)"
+    # Containerd 需要指定命名空间
+    ctr -n=k8s.io image import $IMAGE_PATH/$IMAGE
 done
+
+# 注意，如果安装失败可能是 pause  镜像问题，导入 3.6 版本镜像即可。
