ada7c4fddf727f1065367d38adb15c380dedfc7d
OpenEdgn <ExplodingFKL@gmail.com>
2022-08-06 10:40:33 +0800

    feat: 修复cert-manager 签发问题

--- cluster-deploy/kubernetes/script/02.a-conf-containerd-http-proxy.sh
+++ cluster-deploy/kubernetes/script/02.a-conf-containerd-http-proxy.sh
@@ -3,11 +3,11 @@ mkdir /etc/systemd/system/containerd.service.d ||:
 cat << "EOF" > /etc/systemd/system/containerd.service.d/http_proxy.conf
 [Service]
 # 此处配置为可用的 HTTP 代理地址
-Environment="HTTP_PROXY=http://10.0.0.2:8080"
+Environment="HTTP_PROXY={{var.network.proxy}}"
 # 此处配置为可用的 HTTP 代理地址
-Environment="HTTPS_PROXY=http://10.0.0.2:8080"
+Environment="HTTPS_PROXY={{var.network.proxy}}"
 # 此处配置为跳过代理的地址
-Environment="NO_PROXY=localhost,127.0.0.1,10.0.0.20"
+Environment="NO_PROXY=localhost,127.0.0.1,{{var.global.pub-host}},{{var.network.no-proxy}}"
 EOF
 systemctl daemon-reload
 systemctl restart containerd.service
