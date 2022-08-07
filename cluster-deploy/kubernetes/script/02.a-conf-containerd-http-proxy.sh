81cc6c3f8d7de08ec171d49f50b14f8ad91dc00a
OpenEdgn <ExplodingFKL@gmail.com>
2022-08-07 12:28:53 +0800

    feat: 指定资源占用

--- cluster-deploy/kubernetes/script/02.a-conf-containerd-http-proxy.sh
+++ cluster-deploy/kubernetes/script/02.a-conf-containerd-http-proxy.sh
@@ -7,7 +7,7 @@ Environment="HTTP_PROXY={{var.network.proxy}}"
 # 此处配置为可用的 HTTP 代理地址
 Environment="HTTPS_PROXY={{var.network.proxy}}"
 # 此处配置为跳过代理的地址
-Environment="NO_PROXY=localhost,127.0.0.1,{{var.global.pub-host}},{{var.network.no-proxy}}"
+Environment="NO_PROXY=localhost,127.0.0.1,registry.{{var.global.pub-host}},{{var.network.no-proxy}}"
 EOF
 systemctl daemon-reload
 systemctl restart containerd.service
