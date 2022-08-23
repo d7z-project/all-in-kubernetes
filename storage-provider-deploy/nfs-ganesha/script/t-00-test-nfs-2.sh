167001e6ffc8e349b185ef10fc31871da079a222
ExplodingDragon <ExplodingDragon@users.noreply.github.com>
2022-08-23 09:32:45 +0000

    deploy: 3e14a8a7cc8911b4d04c0eeebe622269a58f0a58

--- storage-provider-deploy/nfs-ganesha/script/t-00-test-nfs-2.sh
+++ storage-provider-deploy/nfs-ganesha/script/t-00-test-nfs-2.sh
@@ -4,4 +4,4 @@ kubectl get deploy,pvc,pods -l mode=test,app=nfs-ganesha
 # 测试结果
 CLUSTER_IP=$(kubectl get services svc-test-nfs-ganesha -o jsonpath='{.spec.clusterIP}')
 POD_NAME=$(kubectl get pod -l app=nfs-ganesha,role=busybox -o jsonpath='{.items[0].metadata.name}')
-kubectl exec $POD_NAME -- wget -qO- http://$CLUSTER_IP
+kubectl exec "$POD_NAME" -- wget -qO- http://"$CLUSTER_IP"
--- zz-document/tools/script/send-image.sh
+++ zz-document/tools/script/send-image.sh
@@ -1,46 +1,66 @@
 #!/usr/bin/env bash
-REMOTE_ADDRESSES=$1
-IMAGES_NAME=$2
 
-if [ ! "$IMAGES_NAME" ] || [ ! "$REMOTE_ADDRESSES" ]; then
+if [ ! "$1" ] || [ ! "$2" ]; then
     echo "help"
     echo ""
-    echo "send-image root@<ip>,root@<ip> image1,image2"
+    echo "send-image root@<ip>,root@<ip> image1,image2,res.yaml,res.yml"
     echo ""
     echo "Example: send-image root@10.0.0.1,root@10.0.0.2 busybox,nginx"
     exit 1
 fi
 
-IFS=',' read -r -a IMAGES <<<"$IMAGES_NAME"
-IFS=',' read -r -a REMOTES <<<"$REMOTE_ADDRESSES"
+# 取配置
+IFS=',' read -r -a REMOTES <<<"$1"
+IFS=',' read -r -a IMAGE_SOURCES <<<"$2"
 
-echo "开始导出镜像"
 FILES=()
-for IMAGE_NAME in ${IMAGES[*]}; do
-    echo "导出 $IMAGE_NAME ."
-    SAVE_IMG_NAME=$(echo "$IMAGE_NAME" | sed s@/@-@g)
-    docker pull "$IMAGE_NAME" || exit 1
-    if [ -f /tmp/"$SAVE_IMG_NAME" ]; then
-        echo "文件 /tmp/$SAVE_IMG_NAME 已存在，跳过导出。"
+IMAGES=()
+for IMAGE_SOURCE in ${IMAGE_SOURCES[*]}; do
+    # shellcheck disable=SC2086
+    if [ -f $IMAGE_SOURCE ] && [[ $IMAGE_SOURCE =~ ^.*(yaml|yml)$ ]]; then
+        # shellcheck disable=SC2002
+        # shellcheck disable=SC2207
+        IMAGES+=($(cat "$IMAGE_SOURCE" | grep "image:" | sed "s/image://g" | xargs echo))
     else
-        docker save -o /tmp/"$SAVE_IMG_NAME" "$IMAGE_NAME"
+        # shellcheck disable=SC2206
+        IMAGES+=($IMAGE_SOURCE)
+    fi
+done
+for IMAGE_NAME in ${IMAGES[*]}; do
+    SAVE_IMG_NAME=$(echo "$IMAGE_NAME" | sed -e s@/@-@g -e s/@/-/g)
+    if [ "$DEBUG" ]; then
+        docker pull "$IMAGE_NAME" || exit 1
+    else
+        docker pull "$IMAGE_NAME" 1>/dev/null || exit 1
+    fi
+    if [ ! -f /tmp/"$SAVE_IMG_NAME" ]; then
+        docker save -o /tmp/"$SAVE_IMG_NAME" "$IMAGE_NAME" 1>/dev/null
     fi
     # shellcheck disable=SC2179
     FILES+=("/tmp/$SAVE_IMG_NAME")
-    echo "导出 $IMAGE_NAME 完成."
-done
-rm -f /tmp/import-script-"$USER" || :
-echo "#!/bin/bash" >/tmp/import-script-"$USER"
-for LOCAL_FILE in ${FILES[*]}; do
-    echo "ctr -n=k8s.io image import $LOCAL_FILE" >>/tmp/import-script-"$USER"
-    echo "/bin/rm -f $LOCAL_FILE" >>/tmp/import-script-"$USER"
 done
 
-echo "开始传输文件"
+SCRIPT_PATH=/tmp/import-script-"$USER"
+
+rm -f "$SCRIPT_PATH" 1>/dev/null || :
+echo "#!/bin/bash" >"$SCRIPT_PATH"
+for LOCAL_FILE in ${FILES[*]}; do
+    # shellcheck disable=SC2129
+    echo "ctr -n=k8s.io image import $LOCAL_FILE" >>"$SCRIPT_PATH"
+    echo "/bin/rm -f $LOCAL_FILE" >>"$SCRIPT_PATH"
+done
+
 for REMOTE_ADDRESS in ${REMOTES[*]}; do
     # shellcheck disable=SC2086
-    scp /tmp/import-script-"$USER" ${FILES[*]} "$REMOTE_ADDRESS":/tmp/
-    ssh "$REMOTE_ADDRESS" "bash /tmp/import-script-$USER"
+    if [ $DEBUG ]; then
+        scp "$SCRIPT_PATH" ${FILES[*]} "$REMOTE_ADDRESS":/tmp/
+        # shellcheck disable=SC2029
+        ssh "$REMOTE_ADDRESS" "bash $SCRIPT_PATH"
+    else
+        scp $SCRIPT_PATH ${FILES[*]} "$REMOTE_ADDRESS":/tmp/ 1>/dev/null
+        # shellcheck disable=SC2029
+        ssh "$REMOTE_ADDRESS" "bash $SCRIPT_PATH" 1>/dev/null
+    fi
 done
 
 /bin/rm ${FILES[*]}
