c4cdd422c7a05154ffd7fc0f0ff443ed6be25b98
Dragon <ExplodingFKL@gmail.com>
2022-09-01 19:48:17 +0800

    docs: 变更措辞“安装”

--- cluster-deploy/pre-deploy/script/00.b-soft-install-openssh.sh
+++ cluster-deploy/pre-deploy/script/00.b-soft-install-openssh.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-# 安装 OpenSSH 软件包
+# 部署 OpenSSH 软件包
 apt-get -y install openssh-server
--- cluster-deploy/pre-deploy/script/00.e-soft-install-vm-tools.sh
+++ cluster-deploy/pre-deploy/script/00.e-soft-install-vm-tools.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-# 安装虚拟化相关的软件包
+# 部署虚拟化相关的软件包
 apt-get -y install qemu-guest-agent cloud-init
